90b9f951ed5fdadceda93c831157306302599b45
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-02-21 23:59:35 +0300

    Add libraries and headers

---
+++ client.c
@@ -0,0 +1,5 @@
+#include <string.h>
+#include <WinSock2.h>
+#include <Windows.h>
+
+#pragma comment(lib,"ws2_32.lib")
---
+++ server.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <string.h>
+#include <WinSock2.h>
+
+#pragma comment(lib,"ws2_32.lib")
