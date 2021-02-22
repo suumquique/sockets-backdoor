ef49a3c47f816ed7ddd5e58d53f8d9ef436c76bc
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-02-22 20:43:06 +0300

    Copy the main structure to the client

--- client.c
+++ client.c
@@ -3,3 +3,37 @@
 #include <Windows.h>
 
 #pragma comment(lib,"ws2_32.lib")
+
+#define HOST "127.0.0.1"
+#define PORT 7777
+
+void initWinsock();
+SOCKADDR_IN createServer();
+
+int main() {
+	initWinsock();
+	SOCKADDR_IN server = createServer();
+}
+
+void initWinsock() {
+	WSADATA wsaData;
+
+	// Номер версии -  2.2
+	WORD winsockVersion = MAKEWORD(2, 2);
+	if (WSAStartup(winsockVersion, &wsaData) != 0) {
+		puts("Winsock library load error");
+		exit(1);
+	}
+}
+
+SOCKADDR_IN createServer() {
+	SOCKADDR_IN server;
+
+	// Адрес, на котором будем слушать соединения
+	server.sin_addr.s_addr = inet_addr(HOST);
+	server.sin_port = PORT;
+	// С какими протоколами будет работать
+	server.sin_family = AF_INET;
+
+	return server;
+}
