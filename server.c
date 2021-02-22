816e4f25244b50961d7bc98a8512d4dc8f0ced83
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-02-22 19:52:39 +0300

    Add incoming connection listener on server

--- server.c
+++ server.c
@@ -10,14 +10,18 @@
 
 void initWinsock();
 SOCKADDR_IN createServer();
+SOCKET getCurrentConnection(SOCKADDR_IN server);
 
 int main() {
 	initWinsock();
 	SOCKADDR_IN server = createServer();
+	SOCKET connection = getCurrentConnection(server);
 }
 
 void initWinsock() {
 	WSADATA wsaData;
+
+	// Номер версии -  2.2
 	WORD winsockVersion = MAKEWORD(2, 2);
 	if (WSAStartup(winsockVersion, &wsaData) != 0) {
 		puts("Winsock library load error");
@@ -27,9 +31,30 @@ void initWinsock() {
 
 SOCKADDR_IN createServer() {
 	SOCKADDR_IN server;
+
+	// Адрес, на котором будем слушать соединения
 	server.sin_addr.s_addr = inet_addr(HOST);
 	server.sin_port = PORT;
+	// С какими протоколами будет работать
 	server.sin_family = AF_INET;
 
 	return server;
+}
+
+SOCKET getCurrentConnection(SOCKADDR_IN server) {
+	int addresslen = sizeof(server);
+
+	SOCKET incomingConnectionsListener = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(incomingConnectionsListener, (SOCKADDR*) &server, addresslen);
+
+	// Слушаем максимально возможное число входящих соединений
+	listen(incomingConnectionsListener, SOMAXCONN);
+
+	SOCKET newConnection = accept(incomingConnectionsListener, (SOCKADDR*)&server, addresslen);
+	if (!newConnection) {
+		puts("Server listener error");
+		exit(1);
+	}
+
+	return newConnection;
 }
