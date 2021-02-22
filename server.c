740ba30826e7b76776f794f3ee19c87ca9ce7ab0
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-02-22 19:41:18 +0300

    Add createServer function: it sets the parameters for the connection

--- server.c
+++ server.c
@@ -5,10 +5,15 @@
 
 #pragma comment(lib,"ws2_32.lib")
 
+#define HOST "127.0.0.1"
+#define PORT 7777
+
 void initWinsock();
+SOCKADDR_IN createServer();
 
 int main() {
 	initWinsock();
+	SOCKADDR_IN server = createServer();
 }
 
 void initWinsock() {
@@ -18,4 +23,13 @@ void initWinsock() {
 		puts("Winsock library load error");
 		exit(1);
 	}
+}
+
+SOCKADDR_IN createServer() {
+	SOCKADDR_IN server;
+	server.sin_addr.s_addr = inet_addr(HOST);
+	server.sin_port = PORT;
+	server.sin_family = AF_INET;
+
+	return server;
 }
