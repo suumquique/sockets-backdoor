360d9746cec22a509e94601fa9e12d541b38c2c4
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-02-22 19:04:42 +0300

    Add winsock initialize function on server

--- server.c
+++ server.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <WinSock2.h>
 
 #pragma comment(lib,"ws2_32.lib")
+
+void initWinsock();
+
+int main() {
+	initWinsock();
+}
+
+void initWinsock() {
+	WSADATA wsaData;
+	WORD winsockVersion = MAKEWORD(2, 2);
+	if (WSAStartup(winsockVersion, &wsaData) != 0) {
+		puts("Winsock library load error");
+		exit(1);
+	}
+}
